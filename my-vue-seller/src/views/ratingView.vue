e9b769450b661c433fabd83f5853569825f1ace4
lofayo <870876711@qq.com>
2018-06-21 08:36:52 +0800

    Signed-off-by: lofayo <870876711@qq.com>

--- my-vue-seller/src/views/ratingView.vue
+++ my-vue-seller/src/views/ratingView.vue
@@ -1,15 +1,205 @@
 <template>
-  <h3>rating</h3>
+  <div class="content">
+    <section class="rating_total">
+      <div class="left">
+        <p>{{seller.score}}</p>
+        <p>综合评分</p>
+        <p>高于周边商家69.2%</p>
+      </div>
+      <div class="right">
+        <div>
+          <p>服务态度</p>
+          <bigStarComponent :score='seller.serviceScore'/>
+          <p class="score">{{seller.serviceScore}}</p>
+        </div>
+        <div>
+          <p>食品质量</p>
+          <bigStarComponent :score='seller.foodScore'/>
+          <p class="score">{{seller.foodScore}}</p>
+        </div>
+        <div>
+          <p>送达时间</p>
+          <p class="score">44分钟</p>
+        </div>
+      </div>
+    </section>
+    <section class="rating_box">
+      <div class="rating_header">
+        <p>全部 {{ratings.length}}</p>
+        <p>满意 {{ratings.filter((item)=> item.rateType === 0).length}}</p>
+        <p>不满意 {{ratings.filter((item)=> item.rateType === 1).length}}</p>
+      </div>
+      <div class="only_look" @click='checkOnlyContent'>
+        <span :class="{only_content:isOnlyContent}"></span>只看有内容的评价
+      </div>
+      <ul class="rating_lists">
+        <li v-for='(rating,index) of ratings' :key='index'>
+          <div class="deliver_mes">
+            <img class="customer_logo" :src="rating.avatar" alt="">
+            <div>
+              <p>{{rating.username}}</p>
+              <bigStarComponent :score='rating.score' />
+            </div>
+            <div>
+              <p>{{timestampToTime(rating.rateTime)}}</p>
+              <p>{{rating.deliveryTime ? rating.deliveryTime : '?'}}分钟送达</p>
+            </div>
+          </div>
+          <div class="rating_mes">
+            {{rating.text}}
+          </div>
+          <div class="rating_thumb">
+            <span v-if='rating.recommend.length > 0' class="thumb_up"></span>
+            <span v-for='item of rating.recommend' class="rating_goods">{{item}}</span>
+          </div>
+        </li>
+      </ul>
+    </section>
+  </div>
 </template>
 
 <script>
+  import bigStarComponent from '@/components/bigStarComponent'
+  import data from '../data.json'
+
 export default {
- 
+  data() {
+    return {
+      ratings: data.ratings,
+      seller: data.seller,
+      isOnlyContent: false
+    }
+  },
+  components:{
+    bigStarComponent
+  },
+  methods: {
+    timestampToTime:function(timestamp) {  
+      let date = new Date(timestamp);//时间戳为10位需*1000，时间戳为13位的话不需乘1000  
+      let Y = date.getFullYear() + '-';  
+      let M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';  
+      let D = date.getDate() + ' ';  
+      let h = date.getHours() + ':';  
+      let m = date.getMinutes(); 
+      return Y+M+D+h+m;  
+    },
+    checkOnlyContent() {
+      this.isOnlyContent = !this.isOnlyContent
+    }  
+  }
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="stylus" scoped>
   @import '../../static/css/mixin.styl'
-
+  .content
+    background: #eee
+    &>section
+      margin-bottom: 0.533333rem
+      border-top: 1px solid #ccc
+      border-bottom: 1px solid #ccc
+      padding: 0.533333rem
+      background-color: white
+    .rating_total
+      padding-bottom: 0.533333rem
+      display: flex
+      .left
+        text-align: center
+        padding-right: 0.533333rem
+        &>p:nth-child(1)
+          font-width: 0.48rem
+          color: yellowgreen
+        &>p:nth-child(3)
+          color: #ccc
+      .right /deep/
+        padding-left: 0.533333rem
+        border-left: 0.026667rem solid #ccc
+        display: flex
+        flex-direction: column
+        justify-content: space-between
+        .star
+          margin-left: 0.133333rem
+          width: 0.32rem
+          height: 0.32rem
+        .score
+          color: yellowgreen
+          margin-left: 0.266667rem
+        &>div
+          display: flex
+    .rating_box
+      .rating_header
+        display: flex
+        padding-bottom: 0.266667rem
+        border-bottom: 1px solid #ccc
+        &>p
+          padding: 0.266667rem
+          margin-right: 0.266667rem
+          background: skyblue
+          color: #fff
+      .only_look
+        padding: 0.266667rem 0
+        color: #999
+        display: flex
+        align-items: center
+        &>span
+          display: inline-block
+          width: 0.533333rem
+          height: 0.533333rem
+          margin-right: 0.266667rem    
+          bg('~images/right')
+        .only_content
+          bg('~images/right-green')
+      .rating_lists
+        &>li
+          border-top: 1px solid #ccc
+          padding: 0.266667rem 0
+          &>div
+            margin-left: 1.333333rem
+            margin-bottom: 0.133333rem
+          .deliver_mes /deep/
+            margin-left: 0
+            margin-bottom: 0.266667rem
+            display: flex
+            align-items: center
+            text-align: center
+            .star
+              width: 0.32rem
+              height: 0.32rem
+              margin-left: 0.133333rem 
+            .customer_logo
+              width: 0.746667rem
+              height: 0.746667rem
+              border-radius: 50%
+              bg('~images/thumbs_up')
+            &>div
+              display: flex
+              flex-direction: column
+            &>div:nth-child(2)
+              margin-left: 0.566667rem
+            &>div:nth-child(3)
+              margin-left: 2.333333rem
+          .rating_mes
+          .rating_thumb
+            display: flex
+            align-items: center
+            flex-wrap: wrap
+            color: #999
+            &>span
+              flex: none
+              display: inline-block
+              margin-right: 0.266667rem
+              padding: 0.066667rem 0.266667rem
+              border: 0.026667rem solid #ccc
+            .thumb_up
+              width: 0.32rem
+              height: 0.32rem
+              border: none
+              bg('~images/thumbs_up')
+            .thumb_down
+              width: 0.32rem
+              height: 0.32rem
+              border: none
+              bg('~images/thumbs_down')
 </style>
--- my-vue-seller/src/views/sellerView.vue
+++ my-vue-seller/src/views/sellerView.vue
@@ -3,10 +3,10 @@
     <section class="section_1">
       <div class="section_1_1">
         <div class="seller_info">
-          <p>粥品香坊（大运村）</p>
+          <p class="title">{{seller.name}}</p>
           <div class="nums">
-            <bigStarComponent />
-            <p>(661)    月售690单</p>
+            <bigStarComponent :score='seller.score' />
+            <p>(661)    月售{{seller.sellCount}}单</p>
           </div>
         </div>
         <div class="collect">
@@ -18,15 +18,15 @@
       <div class="section_1_2">
         <div>
           <p>起送价</p>
-          <p><span class="large">20</span>元</p>
+          <p><span class="large">{{seller.minPrice}}</span>元</p>
         </div>
         <div>
           <p>商家配送</p>
-          <p><span class="large">4</span>元</p>
+          <p><span class="large">{{seller.deliveryPrice}}</span>元</p>
         </div>
         <div>
           <p>平均配送时间</p>
-          <p><span class="large">39</span>分钟</p>
+          <p><span class="large">{{seller.deliveryTime}}</span>分钟</p>
         </div>
       </div>
     </section>
@@ -36,38 +36,27 @@
           公告与活动
         </p>
         <p class="activity_text">
-          粥品香坊其烹饪粥料的秘方源于中国千年古法，在融和现
+          {{seller.bulletin}}
         </p>
       </div>
       <ul class="activity_lists">
-        <li>
-          <span class="icon icon_1"></span>在线支付满28减5，满50减10
-        </li>
-        <li>
-          <span class="icon icon_2"></span>单人精彩赛
-          <span class="icon icon_2"></span>清肺雪梨汤8折抢购
-        </li>
-        <li>
-          <span class="icon icon_3"></span>特价饮品八折抢购
-          <span class="icon icon_3"></span>单人特色套餐
-        </li>
-        <li>
-          <span class="icon icon_4"></span>该商家支持开发票，请在下单时填写好发票抬头
-          <span class="icon icon_4"></span>已加入“外卖保”计划，食品安全保障
+        <li v-for='(support,index) of seller.supports' :key='index'>
+          <span class="icon" :class='generateClass(index+1)'></span>{{support.description}}
         </li>
       </ul>
     </section>
     <section class="section_3">
       <p class="title">商家实景</p>
-      <ul>
-        <li><img src="" alt=""></li>
+      <ul class="img_lists">
+        <li v-for='(pic,index) of seller.pics' :key='index'>
+          <img :src="pic" alt="">
+        </li>
       </ul>
     </section>
     <section class="section_4">
       <p class="title">商家信息</p>
       <ul class="seller_mes">
-        <li>地址：北京市昌平区回龙观西大街龙观置业大厦底商B座</li>
-        <li>营业时间：10:00 - 20:30</li>
+        <li v-for='info of seller.infos' >{{info}}</li>
       </ul>
     </section>
   </div>
@@ -75,9 +64,21 @@
 
 <script>
   import bigStarComponent from '@/components/bigStarComponent'
+  import data from '../data.json'
+
 export default {
+  data() {
+    return {
+      seller: data.seller
+    }
+  },
   components: {
     bigStarComponent
+  },
+  methods: {
+    generateClass(index) {
+      return 'discount_' + index
+    }
   }
  
 }
@@ -97,22 +98,88 @@ export default {
       .hr1px
         border: none
         height: 0.026667rem
-        background-color: #ccc
+        background-color: #ddd
+      .title
+        font-size: 0.373333rem
+        color: rgb(7,17,27)
+        margin-bottom: 0.266667rem
+        font-weight: bold
     .section_1
       .section_1_1
         display: flex
         justify-content: space-between
-        padding-bottom: 0.533333rem
+        padding-bottom: 0.266667rem
         .seller_info
-          .nums
+          .nums /deep/
             display: flex
-            margin-top: 0.533333rem
+            margin-top: 0.266667rem
+            .star
+              width: 0.426667rem
+              height: 0.426667rem
+            .star+.star
+              margin-left: 0.133333rem
         .collect
           display: flex
           flex-direction: column
           align-items: center
+          justify-content:space-around
           .collect_logo
             width: 0.533333rem
             height: 0.533333rem
             bg('~images/heart')
+      .section_1_2
+        display: flex
+        padding-top: 0.266667rem
+        &>div+div
+          border-left: 0.026667rem solid #ddd
+        &>div
+          flex: 1
+          text-align: center
+          .large
+            font-size: 0.533333rem
+    .section_2
+      .activity_text
+        padding-left:0.266667rem
+        font-size: 0.32rem
+        color: rgb(240,20,20)
+        line-height: 0.64rem
+        margin-bottom: 0.533333rem
+      .activity_lists
+        &>li
+          padding: 0.426667rem
+          border-top: 0.026667rem solid rgba(7,17,27,0.1)
+          display:flex
+          align-items: center
+          .icon
+            display: inline-block
+            width: 0.426667rem
+            height: 0.426667rem
+            margin-right: 0.266667rem
+          .discount_1
+            bg_dpr23('~images/decrease_4')
+          .discount_2,.discount_3
+            bg_dpr23('~images/discount_4')
+          .discount_4
+            bg_dpr23('~images/special_4')
+          .discount_5
+            bg_dpr23('~images/guarantee_4')
+    .section_3
+      .img_lists
+        display: flex
+        width: 100%
+        overflow: auto
+        &>li
+          width: 3.2rem
+          height: 2.4rem
+          margin-right: 0.266667rem
+          flex: none
+          &>img
+            width: 100%
+            height: 100%
+    .section_4
+      margin-bottom: 0
+      .seller_mes
+        &>li
+          padding: 0.426667rem
+          border-top: 0.026667rem solid #ddd
 </style>
