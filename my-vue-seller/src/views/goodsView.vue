8ebfc3729133fe56f1ea845e990b91ec21481af2
lofayo <870876711@qq.com>
2018-06-28 08:08:25 +0800

    seller

--- my-vue-seller/src/views/goodsView.vue
+++ my-vue-seller/src/views/goodsView.vue
@@ -8,7 +8,6 @@
     <div class="category_items">
       <div class="item" v-for='(good,index1) of goods'>
         <p class="item_title">{{good.name}}</p>
-<<<<<<< HEAD
         <div class="item_info" v-for='(food,index2) of good.foods'>
           <router-link :to="'/detail/'+index1+'/'+index2">
             <img class="food_img" :src="food.icon" alt="">
@@ -25,22 +24,6 @@
             </div>
             <div class="add_goods"></div>
           </router-link>
-=======
-        <div class="item_info" v-for='food of good.foods'>
-          <img class="food_img" :src="food.icon" alt="">
-          <div class="food_info">
-            <p class="food_title">{{food.name}}</p>
-            <p>
-              <span>月售{{food.sellCount}}份</span>
-              <span>好评率{{food.rating}}%</span>
-            </p>
-            <p class="price">
-              <span class="now_price">￥{{food.price}}</span>
-              <span v-if='food.oldPrice' class="history_price">￥{{food.oldPrice}}</span>
-            </p>
-          </div>
-          <div class="add_goods" @click='parabola'></div>
->>>>>>> 61e6ffd783f680c174876d0294fc432f9ece0e1c
         </div>
       </div>
     </div>
@@ -117,78 +100,42 @@
           background: #eee
           padding-left: 0.533333rem
         .item_info
-          position: relative
-          display: flex
-          align-items: center
-          padding: 0.533333rem 0
-          margin: 0 0.533333rem
-          border-bottom: 0.026667rem solid #ccc
-<<<<<<< HEAD
-        &>li:last-child
-          border: none
-    .category_items
-      height: 66vh
-      overflow: auto
-      flex: 1
-      .item_title
-        color: #666
-        height: 0.733333rem
-        line-height: 0.733333rem
-        border-left: 0.053333rem solid #999
-        background: #eee
-        padding-left: 0.533333rem
-      .item_info
-        &>a
-          position: relative
-          display: flex
-          align-items: center
-          padding: 0.533333rem 0
-          margin: 0 0.533333rem
-          border-bottom: 0.026667rem solid #ccc
-=======
->>>>>>> 9fbf30b803f985badcaef01a96c1c08dccf1115c
-          .food_img
-            width: 1.52rem
-            height: 1.52rem
-            margin-right: 0.266667rem
-          .food_info
+          &>a
+            position: relative
             display: flex
-            flex-direction: column
-            justify-content: space-between
-            flex: none
-            .food_title
-              font-size: 0.373333rem
-              font-weight: bold
-            &>p:nth-child(2)
-              color: #999
-            .price
-              .now_price
-                color: red
+            align-items: center
+            padding: 0.533333rem 0
+            margin: 0 0.533333rem
+            border-bottom: 0.026667rem solid #ccc
+            .food_img
+              width: 1.52rem
+              height: 1.52rem
+              margin-right: 0.266667rem
+            .food_info
+              display: flex
+              flex-direction: column
+              justify-content: space-between
+              flex: none
+              .food_title
                 font-size: 0.373333rem
                 font-weight: bold
-              .history_price
-                font-weight: bold
-                text-decoration: line-through
-          .add_goods
-            position: absolute
-            right: 0
-<<<<<<< HEAD
-            bottom: 0.133333rem
-            width: 1.066667rem
-            height: 1.066667rem
-            margin-left: 0.266667rem
-            bg('~images/add')
-      .item_info:last-child
-        border: none
-=======
-            bottom: 0.266667rem
-            width: 0.64rem
-            height: 0.64rem
-            bg('~images/add')
-            
+              &>p:nth-child(2)
+                color: #999
+              .price
+                .now_price
+                  color: red
+                  font-size: 0.373333rem
+                  font-weight: bold
+                .history_price
+                  font-weight: bold
+                  text-decoration: line-through
+            .add_goods
+              position: absolute
+              right: 0
+              bottom: 0.266667rem
+              width: 0.64rem
+              height: 0.64rem
+              bg('~images/add')
         .item_info:last-child
           border: none
-    
-
->>>>>>> 9fbf30b803f985badcaef01a96c1c08dccf1115c
 </style>
